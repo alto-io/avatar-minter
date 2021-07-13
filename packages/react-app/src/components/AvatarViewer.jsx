d01e07eb9f225471be8f53cfac0c973d690a1ba4
Paul Gadi <paulgadi@gmail.com>
2021-07-13 19:50:06 -0400

    move random avatar generation to avatar viewer

--- packages/react-app/src/components/AvatarViewer.jsx
+++ packages/react-app/src/components/AvatarViewer.jsx
@@ -8,45 +8,40 @@ import ReactJson from "react-json-view";
 /*
   ~ What it does? ~
 
-  Create your own Avatar NFTs!
+  Randomly generate Avatars and view minted ones from the API!
 
   Processes an OpenRaster (.ora) file of generative avatars
   to upload generative avatars to IPFS and create a metadata
   json file usable for tokenUris.
   
-  See packages/avatar/img/ for examples of .ora files.
-  .ora files can be exported from PSD files via GIMP.
+  See public/avatars/avatarimages.ora for examples of .ora files.
+  .ora files can be opened and exported from PSD files via GIMP.
 
   ~ How can I use? ~
 
   Put your .ora file in public/avatars/avatarimages.ora
 
   Client Side:
-  <Avatar/>
+  <AvatarViewer/>
 
   Server Side:
   Copy the metadata json to packages/avatar/data/metadata.json
 
   ~ Features ~
 
-  - Uses ERC-721
+  - easily view random avatars
   - Define avatar specs easily
   - Assumes avatars are pre-minted for now
 */
 
 const STARTING_CONFIG_JSON = {
-    "info": "this will contain config.json that defines the collection specs. Edit as necessary."
+    "Getting Started": "Press ( 😀 New Avatar ), this JSON view will contain the avatar's config.json once it's been loaded."
   };
-
-const STARTING_METADATA_JSON = {
-    "info": "this will contain metadata.json once minting is complete."
-}
   
 export default function AvatarViewer() {
 
-    const [config, canvasRef, canvasWidth, canvasHeight, setNewAvatar] = useAvatar();
     const [configJSON, setConfigJSON] = useState(STARTING_CONFIG_JSON);
-    const [metadataJSON, setMetadataJSON] = useState(STARTING_METADATA_JSON);
+    const [config, canvasRef, canvasWidth, canvasHeight, setNewAvatar] = useAvatar();
 
     const handleClickNewAvatarButton = (event) => {
         setNewAvatar(); 
@@ -82,24 +77,6 @@ export default function AvatarViewer() {
                 <ReactJson
                     style={{ padding: 8 }}
                     src={configJSON}
-                    theme="pop"
-                    enableClipboard={false}
-                    onEdit={(edit, a) => {
-                        setConfigJSON(edit.updated_src);
-                    }}
-                    onAdd={(add, a) => {
-                        setConfigJSON(add.updated_src);
-                    }}
-                    onDelete={(del, a) => {
-                        setConfigJSON(del.updated_src);
-                    }}
-                />
-            </div>
-
-            <div>
-                <ReactJson
-                    style={{ padding: 8 }}
-                    src={metadataJSON}
                     theme="pop"
                     enableClipboard={false}
                     onEdit={(edit, a) => {
