3f18a21a66bebbc2a30e69f9867fe27fd2a0ce70
Paul Gadi <paulgadi@gmail.com>
2021-10-16 14:03:03 -0400

    add more initialization tests

--- packages/hardhat/test/nftTests.js
+++ packages/hardhat/test/nftTests.js
@@ -6,6 +6,9 @@ use(solidity);
 
 describe("NFT Tests", function () {
   let myContract;
+  const promoAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+  const startURI = "https://api.arcadians.io/";
+  const promoAmount = 200;
 
   describe("YourCollectible", function () {
     it("Should deploy YourCollectible", async function () {
@@ -16,11 +19,26 @@ describe("NFT Tests", function () {
 
     describe("on initialize", function () {
       it("promoaddress should have initial tokens", async function () {
-        const promoAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
-        const promoAmount = 200;
         const promoAddressBalance = await myContract.balanceOf(promoAddress);
         expect(promoAddressBalance).to.equal(promoAmount);
       });
+
+      it("totalSupply should start at promoAmount", async function () {
+        expect(await myContract.totalSupply()).to.equal(promoAmount);
+      });
+
+      it("baseURI should be correct", async function () {
+        console.log("base URI: " + startURI);
+        expect(await myContract.baseURI()).to.equal(startURI);        
+      });
+
+      it("tokenURI should be correct", async function () {
+        const id = 15;
+        const tokenURI = startURI + id.toString()
+        console.log("token URI: " + tokenURI);
+        expect(await myContract.tokenURI(id)).to.equal(tokenURI);        
+      });
+
     });
 
 
